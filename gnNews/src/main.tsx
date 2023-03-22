b8d959d84deb527d996e0323fc79f7bc4a28afae
Wojciech Kurzak <wojciechkurzak@sitero.net>
2023-03-22 17:54:06 +0100

    Add routing

--- gnNews/src/main.tsx
+++ gnNews/src/main.tsx
@@ -2,9 +2,27 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import { Provider } from 'react-redux'
 import { store } from './redux/store'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    children: [
+      {
+        path: '/',
+        element: null,
+      },
+      {
+        path: '/country/:country',
+        element: null,
+      },
+    ],
+  },
+])
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-	<Provider store={store}>
-		<App />
-	</Provider>
+  <Provider store={store}>
+    <RouterProvider router={router} />
+  </Provider>
 )
