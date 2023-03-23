521b12fb4b1028d1f31fa33a909b45f39a1f9534
Wojciech Kurzak <wojciechkurzak@sitero.net>
2023-03-23 19:08:27 +0100

    Fix menu not closing on country choose

--- gnNews/src/components/navigation/NavigationListItem.tsx
+++ gnNews/src/components/navigation/NavigationListItem.tsx
@@ -1,5 +1,7 @@
 import { NavigationListItemProps } from '../../types/navigation-types'
-import { useNavigate, NavLink } from 'react-router-dom'
+import { NavLink } from 'react-router-dom'
+import { useAppDispatch } from '../../redux/hooks'
+import { changeMenuVisible } from '../../redux/features/menu-visible-slice'
 import '../../styles/navigation/NavigationListItem.scss'
 
 const NavigationListItem = ({
@@ -7,12 +9,17 @@ const NavigationListItem = ({
   imageUrl,
   alpha,
 }: NavigationListItemProps) => {
-  const navigate = useNavigate()
+  const dispatch = useAppDispatch()
+
+  const handleCloseMenu = (): void => {
+    dispatch(changeMenuVisible(false))
+  }
 
   return (
     <li className='navigation-list-item'>
       <NavLink
         to={`/country/${alpha.toLowerCase()}`}
+        onClick={handleCloseMenu}
         className={({ isActive }) => (isActive ? 'active' : undefined)}
       >
         <img src={imageUrl} alt='flag' />
