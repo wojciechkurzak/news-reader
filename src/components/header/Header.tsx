ee88569ea251628bb940afabd4b35debbf97e881
Wojciech Kurzak <wojciechkurzak@sitero.net>
2023-05-02 14:26:36 +0200

    Delete header modal

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,19 +1,14 @@
-import { useState } from 'react'
 import { useAppDispatch, useAppSelector } from '../../redux/hooks'
 import { changeArticleView } from '../../redux/features/article-view-slice'
 import { ArticleViewStateType } from '../../types/store-types'
 import { FaListUl, FaBars } from 'react-icons/fa'
 import { HiSquares2X2 } from 'react-icons/hi2'
-import { BsQuestionLg } from 'react-icons/bs'
 import { useNavigate } from 'react-router-dom'
 import HeaderButton from './HeaderButton'
-import HeaderModal from './HeaderModal'
 import { changeMenuVisible } from '../../redux/features/menu-visible-slice'
 import '../../styles/header/Header.scss'
 
 const Header = () => {
-  const [modal, setModal] = useState<boolean>(false)
-
   const navigate = useNavigate()
 
   const articleView = useAppSelector((state) => state.articleView.type)
@@ -28,41 +23,29 @@ const Header = () => {
     dispatch(changeMenuVisible(!menuVisible))
   }
 
-  const handleOpenModal = (): void => {
-    setModal(true)
-  }
-
-  const handleCloseModal = (): void => {
-    setModal(false)
-  }
-
   const handleArticleViewChange = (type: ArticleViewStateType): void => {
     dispatch(changeArticleView(type))
   }
 
   return (
-    <>
-      <header>
-        <div className='header-title'>
-          <HeaderButton icon={<FaBars />} onClick={handleToggleMenu} />
-          <h1 onClick={handleNavigateHome}>gnNews</h1>
-        </div>
-        <div className='header-buttons'>
-          <HeaderButton
-            icon={<FaListUl />}
-            active={articleView === 'list' ? 'active' : undefined}
-            onClick={() => handleArticleViewChange('list')}
-          />
-          <HeaderButton
-            icon={<HiSquares2X2 />}
-            active={articleView === 'grid' ? 'active' : undefined}
-            onClick={() => handleArticleViewChange('grid')}
-          />
-          <HeaderButton icon={<BsQuestionLg />} onClick={handleOpenModal} />
-        </div>
-      </header>
-      <HeaderModal open={modal} onClose={handleCloseModal} />
-    </>
+    <header>
+      <div className='header-title'>
+        <HeaderButton icon={<FaBars />} onClick={handleToggleMenu} />
+        <h1 onClick={handleNavigateHome}>gnNews</h1>
+      </div>
+      <div className='header-buttons'>
+        <HeaderButton
+          icon={<FaListUl />}
+          active={articleView === 'list' ? 'active' : undefined}
+          onClick={() => handleArticleViewChange('list')}
+        />
+        <HeaderButton
+          icon={<HiSquares2X2 />}
+          active={articleView === 'grid' ? 'active' : undefined}
+          onClick={() => handleArticleViewChange('grid')}
+        />
+      </div>
+    </header>
   )
 }
 
